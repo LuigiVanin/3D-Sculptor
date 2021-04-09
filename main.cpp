f810d2a771068e7766ed63440339547b0379021c
LuigiVanin <luisfvaninmartins@gmail.com>
2021-04-09 00:11:02 -0300

    interpreter finished

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 
     std::vector<GeometricFigure*> figs;
 
-    figs = Inter.compile("text_scripts/test.txt");
+    figs = Inter.compile("text_scripts/all.txt");
 
     model = new Sculptor(Inter.getDimX(), Inter.getDimY(), Inter.getDimZ());
 
@@ -26,7 +26,7 @@ int main()
 
     model->putVoxel(Inter.getDimX() - 1, Inter.getDimY() - 1, Inter.getDimZ() - 1);
     model->putVoxel(0, 0, 0);
-    model->writeOFF("models/script/test.off");
+    model->writeOFF("models/script/all.off");
 
     for(int i = 0; i < (int) figs.size(); i++){
         delete figs[i];
