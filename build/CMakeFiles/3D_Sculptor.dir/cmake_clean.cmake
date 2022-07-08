c49ad2e15ad8a99ec63843d69c36792a0f578a49
Luis Felipe Vanin <luisfvaninmartins@gmail.com>
2022-07-07 22:17:22 -0300

    aceitando caminhos de arquivos de script e modelos via linha de comando

---
+++ build/CMakeFiles/3D_Sculptor.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "3D_Sculptor"
+  "3D_Sculptor.pdb"
+  "CMakeFiles/3D_Sculptor.dir/main.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/CutBox.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/CutDisc.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/CutEllipsoid.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/CutSphere.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/CutVoxel.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/Interpreter.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/PutBox.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/PutDisc.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/PutEllipsoid.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/PutSphere.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/PutVoxel.cpp.o"
+  "CMakeFiles/3D_Sculptor.dir/src/Sculptor.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D_Sculptor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
